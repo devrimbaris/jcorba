bd2d8142b31b8333bc64da4b131fe4dd4fc6ea99
devrim baris acar <devrimbaris@yahoo.com>
2015-01-27 23:40:18 +0200

    *** empty log message ***

--- src/jcorba/core.clj
+++ src/jcorba/core.clj
@@ -22,19 +22,77 @@
 (get-mp3-url "grandma")
 
 
-(defn get-aa-teaser []
-  (let [url  (str  "http://www.aa.com.tr/tr" )
+(defn get-aa-teasers []
+  (let [url  (str  "http://www.aa.com.tr/tr/" )
         working-set (if-let [doc (try  (.get (Jsoup/connect url)) (catch Exception e1 nil))]
                       (let [teasers  (.select doc "ol.teasers li h3 a")]
                         teasers
                         ))]
                                         
-    (map #(.attr (.select % "a") "href") working-set)
+    (map #(assoc {}
+            :url (str url (.attr (.select % "a") "href"))
+            :title (.html (.select % "a") )
+            :description ""
+            ) working-set)
+    ))
+
+(get-aa-teasers)
+
+
+(defn get-cihan-teasers []
+  (let [url  (str  "http://www.cihan.com.tr/home/" )
+        working-set (if-let [doc (try  (.get (.userAgent  (Jsoup/connect url) "Mozilla"))
+                                       (catch org.jsoup.HttpStatusException e1  (.getStatusCode e1)))]
+                      (.select  doc "#last-news li"))]
+    working-set
+    (map #(assoc {}
+            :url (str url (.attr (.select % "a") "href"))
+            :title (.attr (.select % "a") "title")
+            
+            ) working-set)
+    ))
+
+(get-cihan-teasers)
+
+
+(defn get-iha-rss []
+  (let [url  (str  "http://www.iha.com.tr/rss.php")
+        working-set (if-let [doc (try  (.get (Jsoup/connect url) ) (catch org.jsoup.HttpStatusException e1  (.getStatusCode e1)))]
+                      doc)
+        items (.select working-set "link")]
+    (map #(assoc {}
+            :url (.html (.select % "link")) 
+            :title (.html (.select % "title") )
+            :description (.html (.select % "description") )
+            :category (.html (.select % "category") )
+            ) items)
+    items
+    
     ))
 
 
-(get-aa-teaser)
+(defn get-iha-rss []
+  (let [url  (str  "http://www.iha.com.tr/rss.php")
+        working-set (if-let [doc (try  (.get (Jsoup/connect url)) (catch org.jsoup.HttpStatusException e1  (.getStatusCode e1)))]
+                      doc)
+        items (.select working-set "item")]
+    (map #(assoc {}
+            :url (.select % ":contains(tehdit)") 
+            :title (.html (.select % "title") )
+            :description (.html (.select % "description") )
+            :category (.html (.select % "category") )
+            ) items)
+    working-set
+    ))
 
+
+(get-iha-rss)
+
+
+
+
+
+;#last-news ul.last-news-list
 
 
 
